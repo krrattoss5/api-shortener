7fbf8f2b5fc556c89b054318f015b1f11dcfb56a
krrattoss5 <krrattoss5@gmail.com>
2024-06-06 22:16:11 -0500

    (add/prismaORM)

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
 const app = express()
 const port = 3001
 
-app.use('/', (req, res) => {
-  res.status(200).json({name: 'Jhon Doe'})
+// app.name('api-shortener')
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.post('/', (req, res) => {
+  const {url} = req.body
+  const shortUrl = Math.random().toString(36).substring(2,5)
+
+  res.status(200).json({url,shortUrl})
 })
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`))
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   return res.status(200).json({name: 'Jhon Doe'})
 })
 
