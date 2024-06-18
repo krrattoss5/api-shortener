68105099ccb4f8c8c323def33cfa37e18fcf69aa
krrattoss5 <krrattoss5@gmail.com>
2024-06-17 23:45:52 -0500

    (remove: the method OPTIONS. add: the method PATCH. )

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: '*',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 }));
 
 app.use('/',router)
